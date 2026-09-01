0a6536afadae9dfe0a3606354b5584ff4ca3d9b0
Ali Osman Kara <aliosmankara111@gmail.com>
2026-09-01 13:04:39 -0400

    chore(engine): scaffold pure python package

---
+++ engine/src/dekopen_engine/__init__.py
@@ -0,0 +1,10 @@
+"""Pure deterministic engine package.
+
+SHOT-01 intentionally exposes package identity only. Calculation contracts start
+in SHOT-03.
+"""
+
+PACKAGE_NAME = "dekopen-engine"
+__version__ = "0.1.0"
+
+__all__ = ["PACKAGE_NAME", "__version__"]
